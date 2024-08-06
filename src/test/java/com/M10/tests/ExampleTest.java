4ef06d0b4e55a725d4b7871bcc285f68c93d7fa7
Mohs10 Technologies <104430393+Mohs10@users.noreply.github.com>
2024-08-06 19:54:00 +0530

    Create ExampleTest.java

---
+++ src/test/java/com/M10/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.M10.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+    
+    @Test
+    public void testSample() {
+        Assert.assertTrue(true);
+    }
+}
